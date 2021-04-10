1d2dc20b266773abfa617563dee561e804080f75
kazumawada <kazumawadaa@gmail.com>
2021-04-10 11:04:01 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 /* <i class="fab fa-github"></i> */
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import vuetify from './plugins/vuetify';
+
 
 library.add(fab )
+// Vue.use(Vuetify);
 
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
@@ -18,5 +22,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
